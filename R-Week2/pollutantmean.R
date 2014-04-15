72ec4e22370429e6aacb85ba0e525c0b6d924dfb
Kyle Pace <kylepace03@gmail.com>
2014-04-14 21:06:42 -0400

    Week 2 exercise 1

---
+++ R-Week2/pollutantmean.R
@@ -0,0 +1,17 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+	## 'directory' is a character vector of length 1 indicating
+	## the location of the CSV files
+	for (file in list.files(directory)) {
+		fileName <- paste0(directory, '/', file)
+		if (exists("monitors")) {
+			monitors <- rbindlist(list(monitors, read.csv(fileName)))
+		} else {
+			monitors <- read.csv(fileName)
+		}
+	}
+
+	onlyPollutants <- subset(mons, , select = c(pollutant, "ID"))
+	noNa <- onlyPollutants[complete.cases(onlyPollutants),]
+	subSetted <- subset(noNa, ID %in% id, select = pollutant)
+	mean(subSetted[[pollutant]])
+}
